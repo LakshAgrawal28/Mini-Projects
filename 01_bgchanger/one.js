e97a1934ba1992af8beee97286b9087341c6f4ea
LakshAgrawal28 <sahilvinod544@gmail.com>
2025-06-17 17:46:53 +0530

    1st project

---
+++ 01_bgchanger/one.js
@@ -0,0 +1,9 @@
+document.querySelector("#myButton").addEventListener("click", (e) => {
+    document.body.style.backgroundColor = "#fff";
+})
+document.querySelector("#myButton2").addEventListener("click", (e) => {
+    document.body.style.backgroundColor = "#212121";
+})
+document.querySelector("#myButton3").addEventListener("click", (e) => {
+    document.body.style.backgroundColor = "#123AAA";
+})
